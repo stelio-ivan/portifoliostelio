7166accad5e50e0db9e47269c9a805e34f494b5a
sPALLA <stelioivan3@gmail.com>
2020-07-08 13:18:12 +0100

    first commit

---
+++ src/main/java/pt/meuportifolio/stelio/StelioApplication.java
@@ -0,0 +1,13 @@
+package pt.meuportifolio.stelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StelioApplication.class, args);
+	}
+
+}
